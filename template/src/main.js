12729509ea9e18d35e453a3014ea044a67c38733
yinhang <yinhang@dankegongyu.com>
2019-04-12 18:13:12 +0800

    template demo

--- template/src/main.js
+++ template/src/main.js
@@ -1,18 +1,37 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
+import store from './store'
 import router from './router'
+
+import './icons' // icon
+import './permission' // permission control
+
+import * as filters from './filters' // global filters
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: {
-    App
-  },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
