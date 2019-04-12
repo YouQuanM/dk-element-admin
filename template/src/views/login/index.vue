b5c5a5ebb0775a036c8ddd7ceb75df9898dde599
yinhang <yinhang@dankegongyu.com>
2019-04-12 19:57:01 +0800

    fix

--- template/src/views/login/index.vue
+++ template/src/views/login/index.vue
@@ -4,7 +4,7 @@
 
       <div class="title-container">
         <h3 class="title">
-          {{ $t('login.title') }}
+          {{ 'login.title' }}
         </h3>
       </div>
 
@@ -15,7 +15,7 @@
         <el-input
           ref="username"
           v-model="loginForm.username"
-          :placeholder="$t('login.username')"
+          :placeholder="'login.username'"
           name="username"
           type="text"
           auto-complete="on"
@@ -46,29 +46,29 @@
       </el-tooltip>
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">
-        {{ $t('login.logIn') }}
+        {{ 'login.logIn' }}
       </el-button>
 
       <div style="position:relative">
         <div class="tips">
-          <span>{{ $t('login.username') }} : admin</span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+          <span>{{ 'login.username' }} : admin</span>
+          <span>{{ 'login.password' }} : {{ 'login.any' }}</span>
         </div>
         <div class="tips">
           <span style="margin-right:18px;">
-            {{ $t('login.username') }} : editor
+            {{ 'login.username' }} : editor
           </span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+          <span>{{ 'login.password' }} : {{ 'login.any' }}</span>
         </div>
 
         <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          {{ $t('login.thirdparty') }}
+          {{ 'login.thirdparty' }}
         </el-button>
       </div>
     </el-form>
 
-    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog">
-      {{ $t('login.thirdpartyTips') }}
+    <el-dialog :title="'login.thirdparty'" :visible.sync="showDialog">
+      {{ 'login.thirdpartyTips' }}
       <br>
       <br>
       <br>
